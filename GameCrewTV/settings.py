e4814ed52474fbd264ec9ee01406f12682f872ca
AngelCzu <127989632+AngelCzu@users.noreply.github.com>
2023-10-24 00:15:32 -0300

    asdas

--- GameCrewTV/settings.py
+++ GameCrewTV/settings.py
@@ -70,8 +70,6 @@ TEMPLATES = [
     },
 ]
 
-LOGIN_REDIRECT_URL = "inicio"
-
 WSGI_APPLICATION = 'GameCrewTV.wsgi.application'
 
 
@@ -121,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+APPEND_SLASH=False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
