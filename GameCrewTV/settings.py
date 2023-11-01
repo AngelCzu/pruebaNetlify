eea40cc337460cf37d41ed8d63a2db25c94a66a5
AngelCzu <a.cea@duocuc.cl>
2023-10-31 22:40:35 -0300

    sdsd

--- GameCrewTV/settings.py
+++ GameCrewTV/settings.py
@@ -126,7 +126,7 @@ LOGOUT_REDIRECT_URL = '/inicio'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'app/tvapp/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 APPEND_SLASH=False
