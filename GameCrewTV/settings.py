6c1d1d793144edf986c168899a2b74da5e4930a6
AngelCzu <a.cea@duocuc.cl>
2023-11-02 22:49:34 -0300

    hhhhhhhhhhh

--- GameCrewTV/settings.py
+++ GameCrewTV/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'GameCrewTV.wsgi.application'
+WSGI_APPLICATION = 'GameCrewTV.wsgi'
 
 
 # Database
@@ -131,7 +131,7 @@ LOGOUT_REDIRECT_URL = '/inicio'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
