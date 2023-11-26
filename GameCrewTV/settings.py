906fb63c1f1d7f9ebca3f367eafb223dada27d4e
AngelCzu <a.cea@duocuc.cl>
2023-11-26 00:07:05 -0300

    veamos que pasa

--- GameCrewTV/settings.py
+++ GameCrewTV/settings.py
@@ -79,10 +79,12 @@ WSGI_APPLICATION = 'gamecrewtv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
 
